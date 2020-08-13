f74bb397b5538eb96bc70fbdc3f7bf35cba4b7ba
Prerona Mukherjee <prerona24@gmail.com>
2020-08-13 01:20:02 -0500

    Ajax Calls for titles and mood selector

---
+++ script.js
@@ -0,0 +1,83 @@
+
+
+var title = "The Adventures of Winnie the Pooh"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+var title = "Carrie"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+var title = "A Brief History of Time"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+var title = "Pride and Prejudice"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+
+var title = "Eat,Pray,Love"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+
+
+var title= "Born a Crime"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
+
+var title = "The Curious Case of Benjamin Button"
+
+var goodreadsQueryURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/title.xml?key=cU7MkZMEBPNFmw5qMfbw&title=" + title;
+$.ajax({
+    url: goodreadsQueryURL,
+    method: "GET"
+}).then(function (nextResponse) {
+    console.log(nextResponse)
+});
+
+
